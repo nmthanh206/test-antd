bdacb8077798ef978a91e867ea89a87c17b351fd
Thanh <nmthanh20006@gmail.com>
2021-09-04 04:11:14 +0700

    ok xong

---
+++ src/pages/admin/productlist/[pageNumber].js
@@ -0,0 +1,24 @@
+//tam thoi
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import TableListProducts from "@/components/TableListProducts";
+import { useRouter } from "next/router";
+
+const ProductListScreen = () => {
+   const router = useRouter();
+   const user = useSelector((state) => state.userLogin.user);
+
+   useEffect(() => {
+      if (!user || !user.isAdmin) {
+         router.push("/login");
+      }
+   }, [router, user]);
+   return (
+      <div>
+         <h1 className="text-center">PRODUCTS</h1>
+         <TableListProducts userInfo={user} />
+      </div>
+   );
+};
+
+export default ProductListScreen;
