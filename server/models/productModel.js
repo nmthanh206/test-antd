0d05156ab38f67e6c3fc364ff7591b6b2581297a
Thanh <nmthanh20006@gmail.com>
2021-09-08 16:50:34 +0700

    delete name review

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const reviewSchema = mongoose.Schema(
    {
-      name: { type: String, required: true },
+      // name: { type: String, required: true },
       rating: { type: Number, required: true },
       comment: { type: String, required: true },
       user: {
